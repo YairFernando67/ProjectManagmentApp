fe4f3053bf2c949641bcd0768d56ebcd5b5728cd
Yair fernando <yair.facio11@gmail.com>
2019-11-20 18:54:35 -0600

    Fixing the routes for Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  as :user do 
+  as :user do
     put '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation
   end
 
-  devise_for :users, controlers: { 
+  devise_for :users, controllers: { 
     registrations: 'registrations',
     confirmations: 'confirmations'
   }
